96c4a6428d38d99fb6c4ec37be3f128bac7f53a5
khaivnguyen02 <khaivnguyen02@gmail.com>
2023-01-31 19:04:11 -0800

    add lecture_11_demo

---
+++ Demo/Lecture_11_Demo/arith.s
@@ -0,0 +1,36 @@
+	.file	"arith.c"
+	.text
+	.globl	arith
+	.type	arith, @function
+arith:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	leaq	(%rsi,%rsi,2), %rax
+	salq	$4, %rax
+	leaq	4(%rdi,%rax), %rax
+	addq	%rsi, %rdi
+	addq	%rdx, %rdi
+	imulq	%rdi, %rax
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	arith, .-arith
+	.ident	"GCC: (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
